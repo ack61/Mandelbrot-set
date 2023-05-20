249862f4e7a43bb0a77ebe7a9f8d52702e4762fd
ack61 <85378035+ack61@users.noreply.github.com>
2023-05-20 10:26:51 +0900

    高速化
    それぞれの行を一旦文字列に格納してからまとめて出力するようにした

--- console/java/normal/Main.java
+++ console/java/normal/Main.java
@@ -1,10 +1,10 @@
 public class Main{
 	public static void main(String args[]){
-/*		int rep = 100;
+		int rep = 100;
 
 		double aspect = 2.0;
 
-		int pixY = 300;
+		int pixY = 400;
 		int pixX = (int)(pixY * aspect);
 
 		double cx = -1.0;
@@ -20,6 +20,7 @@ public class Main{
 		double ny = sy;
 		for(int i = 0; i < pixY; i++){
 			double nx = sx;
+			String line = "";
 			for(int j = 0; j < pixX; j++){
 				double x = nx;
 				double y = ny;
@@ -37,63 +38,15 @@ public class Main{
 					count++;
 				}
 				if(isIn){
-					System.out.print("#");
+					line += "#";
 				}else{
-					System.out.print(".*:".toCharArray()[count%3]);
+					line += ".*/".toCharArray()[count%3];
 				}
 				nx += dx;
 			}
-			System.out.println();
+			System.out.println(line);
 			ny -= dy;
 
 		}
-*/
-		int rep = 100;
-
-		double a = 2.0;
-
-		int Y = 300;
-		int X = (int)(Y * a);
-
-		double A = -1.0;
-		double cy = 0.0;
-		double rx = 2.0;
-		double ry = rx * Y / X * a;
-		double dx = rx / X;
-		double dy = ry / Y;
-
-		double sx = A - rx / 2;
-		double sy = cy + ry / 2;
-
-		double ny = sy;
-		for(int i = 0; i < Y; i++){
-			double nx = sx;
-			for(int j = 0; j < X; j++){
-				double x = nx;
-				double y = ny;
-				int count = 0;
-				boolean isIn = true;
-				for(int k = 0; k < rep; k++){
-					if(x*x + y*y > 4){
-						isIn = false;
-						break;
-					}
-					double hx = x*x - y*y + nx;
-					double hy = 2.0*x*y + ny;
-					x = hx;
-					y = hy;
-					count++;
-				}
-				if(isIn){
-					System.out.print("#");
-				}else{
-					System.out.print(".*:".toCharArray()[count%3]);
-				}
-				nx += dx;
-			}
-			System.out.println();
-			ny -= dy;
-		}
-
 	}
 }
